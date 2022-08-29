2d9c5795934a65c933f6a0c1ba9edeb20cb25bfa
Vlad Magdenko <vladearth7@gmail.com>
2022-08-29 11:53:41 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    filename = None
+
+    def __init__(self, file_name: str):
+        self.filename = file_name
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        file_path = "E:\\projects\\py-clean-up-file\\file.txt"
+        os.remove(file_path)
