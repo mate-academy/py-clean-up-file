2517c704adbcb200050c00ec7c851bfd5ff376de
Vlad Magdenko <vladearth7@gmail.com>
2022-08-29 12:00:41 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,14 +2,11 @@ import os
 
 
 class CleanUpFile:
-    filename = None
-
-    def __init__(self, file_name: str):
-        self.filename = file_name
+    def __init__(self, filename: str):
+        self.filename = filename
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        file_path = "E:\\projects\\py-clean-up-file\\file.txt"
-        os.remove(file_path)
+        os.remove(self.filename)
