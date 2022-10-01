3795a0916cb152d8e33cb23ec1c57845797a4463
Roman Kolmakov <cerne313@gmail.com>
2022-10-01 16:41:53 +0300

    Solve the task

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
+from os import remove
+import time
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        remove(self.filename)
