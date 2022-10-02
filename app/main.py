c48540a1414a23e7de2587b1dda4aeb6a3bd282a
Alexander <alexzubkoff123@gmail.com>
2022-10-03 00:32:23 +0300

    Created custom file context manager with file remove

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str, mode: str = "w") -> None:
+        self.filename = filename
+        self.mode = mode
+        self.file = None
+
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.file.close()
+        os.remove(self.filename)
