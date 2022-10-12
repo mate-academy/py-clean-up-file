2b7e991ec626fe99f4702e2ba8dba5cb6b8f5690
Iurii Lobunets <iurii@lobunets.com.ua>
2022-10-12 15:19:16 +0300

    Solution of py-clean-up-file

--- app/main.py
+++ app/main.py
@@ -1,3 +1,20 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> object:
+        return self
+
+    def __exit__(self, exc_type: object, exc_val: object,
+                 exc_tb: object) -> None:
+        if os.path.isfile(self.filename):
+            os.remove(self.filename)
+
+
+if __name__ == "__main__":
+    with CleanUpFile("file.txt"):
+        with open("file.txt", "w") as file_obj:
+            file_obj.write("Hello Mate!")
