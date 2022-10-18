6380dd2896babdf69bb8c89bbdf8e7b0012e4b2b
Andrey Losev <andry07070712@gmail.comless.gitconfiggit config --global user.email andry07070712@gmail.com>
2022-10-18 13:30:10 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
-class CleanUpFile:
-    # write your code here
-    pass
+import os
+
+
+class CleanUpFile(object):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        os.remove(self.filename)
+
+
+
+
