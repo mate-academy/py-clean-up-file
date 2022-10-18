54eb83617d1659370f2ceb7dae0207fb82bc4c75
Andrey Losev <andry07070712@gmail.comless.gitconfiggit config --global user.email andry07070712@gmail.com>
2022-10-18 13:33:06 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,15 +2,14 @@ import os
 
 
 class CleanUpFile(object):
-    def __init__(self, filename):
+    def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> callable:
         return self
 
-    def __exit__(self, exc_type, exc_value, exc_traceback):
+    def __exit__(self,
+                 exc_type: None,
+                 exc_value: None,
+                 exc_traceback: None) -> None:
         os.remove(self.filename)
-
-
-
-
