5fe0a7cf20a35f2dc379a3d54539e8aab2299588
Nailia Mamiedova <nailia.mamiedova.work@gmail.com>
2022-10-22 13:20:31 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
+import os
+from typing import TextIO
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, file_name: str) -> None:
+        self.filename = file_name
+
+    def __enter__(self) -> TextIO:
+        self.file = open(self.filename)
+        return self.file
+
+    def __exit__(self, typo: str, value: str, traceback: str) -> None:
+        self.file.close()
+        os.remove(self.filename)
