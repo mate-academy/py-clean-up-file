02b95bc3582e6387dee3d0dd931ec068caa07ef7
Anton Chumak <Chumak7377@gmail.com>
2022-10-24 18:48:12 +0300

    Solution Custom Context Manager

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
+from __future__ import annotations
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> CleanUpFile:
+        if not os.path.isfile(self.filename):
+            return self
+
+    def __exit__(self, exc_type: None, exc_val: None,
+                 exc_tb: str) -> None:
+        os.remove(self.filename)
