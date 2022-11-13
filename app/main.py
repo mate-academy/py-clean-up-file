49c8621ae3ef71bb53db8383e8ca097ba5db1612
Danylo Tiutiushkin <48014331+danylott@users.noreply.github.com>
2022-11-13 12:18:03 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+        self.file = None
+
+    def __enter__(self):
+        self.file = open(self.filename, "r")
+        return self.file.read()
+
+    def __exit__(self, exc_type, exc_value, exc_traceback) -> None:
+        self.file.close()
+        os.remove(self.filename)
+
+
+if __name__ == '__main__':
+    with CleanUpFile("file.txt"):
+        with open("file.txt", "w") as file:
+            file.write("Hello Mate!")
