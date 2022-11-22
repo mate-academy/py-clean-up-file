64dbf540a51e744152f5eff8ce949c1e9ca7880f
soobig666 <darkvader.ukr.net@gmail.com>
2022-11-22 22:43:35 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ import os
 
 
 class CleanUpFile:
-    def __init__(self, filename):
+    def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> object:
         return self
 
     def __exit__(self, exc_type: type,
