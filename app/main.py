6770a365ceb5773f244f8449de7c24b1859504b2
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-12-09 14:54:21 +0200

    flake 8 3

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
-from os import remove
+import os
 
 
 class CleanUpFile:
-    def __init__(self, file_name: str) -> None:
-        self.file_name = file_name
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
 
     def __enter__(self) -> object:
         return self
 
     def __exit__(self, exc_type: any, exc_val: any, exc_tb: any) -> None:
-        remove(self.file_name)
+        os.remove(self.filename)
