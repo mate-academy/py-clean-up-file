95db6300251bc592bab536e5b3699bb1e0418495
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-12-09 14:39:15 +0200

    create context manager

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, file_name: str) -> None:
+        self.file_name = file_name
+    
+    def __enter__(self) -> object:
+        return self
+
+    def __exit__(self, exc_type: any, exc_val: any, exc_tb: any) -> None:
+        os.remove(self.file_name)
