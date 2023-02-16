2450407ef4a4c2d71b23573f40d3fab045ce16d2
David Gluschenko <davidglusch@gmail.com>
2023-02-16 13:11:20 +0200

    1

--- app/main.py
+++ app/main.py
@@ -1,3 +1,20 @@
+from typing import TextIO
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> object:
+        self.file = open(self.filename, "a+")
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.file.close()
+        os.remove(self.filename)
+
+
+with CleanUpFile("file.txt"):
+    with open("file.txt", "w") as file:
+        file.write("Hello Mate!")
