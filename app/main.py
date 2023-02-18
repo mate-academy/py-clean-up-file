5622722b403349cdf597275b3c55ec4ee41675a4
usrofgh <mailforgimail@gmail.com>
2023-02-18 19:47:42 +0200

    flake fixes

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
-import os
+from __future__ import annotations
+from os import remove
+from typing import Any
 
 
 class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> CleanUpFile:
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        os.remove(self.filename)
+    def __exit__(
+            self,
+            exc_type: Any,
+            exc_val: Any,
+            exc_tb: Any
+    ) -> None:
+        remove(self.filename)
