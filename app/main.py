80b8afcbc4731557f1c28e7cbc699ca71405d1d1
Oleksandr Hryshchenko <oleksandr.grk@gmail.com>
2023-02-20 07:13:09 +0200

    add annotation

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing import Any
 import os
 
 
@@ -12,7 +13,7 @@ class CleanUpFile:
         self.file = open(self.filename, "w")
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
         self.file.close()
         os.remove(self.filename)
 
