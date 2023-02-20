a388f5aa59c9d6c675279c7b373004337980863a
Misha Vovchok <mivvaw@gmail.com>
2023-02-20 19:05:25 +0200

    sol

--- app/main.py
+++ app/main.py
@@ -1,14 +1,19 @@
-import os
+from __future__ import annotations
 from typing import Any
+import os
+
 
 class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-
-    def __enter__(self) -> Any:
+    def __enter__(self) -> CleanUpFile:
         return self
 
-
-    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
+    def __exit__(
+            self,
+            exc_type: Any,
+            exc_val: Any,
+            exc_tb: Any
+    ) -> None:
         os.remove(self.filename)
