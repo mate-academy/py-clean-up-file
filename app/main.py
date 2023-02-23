e21343c9589032878851f224438c291345703f0f
sdobzhynetskyi <sergii.dobzhynetskyi@gmail.com>
2023-02-23 17:18:47 +0200

    solved flake 8

--- app/main.py
+++ app/main.py
@@ -11,6 +11,6 @@ class CleanUpFile:
         self.file = open(self.filename,  "w")
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+    def __exit__(self, exc_type: str, exc_val: str, exc_tb: str) -> None:
         self.file.close()
         os.remove(self.filename)
