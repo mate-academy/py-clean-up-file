10bd67875cb285e5f740422614423cb8e01765af
Vladimir Titarenko <tytarenkova@gmail.com>
2023-02-25 16:23:32 +0300

    annotations were added

--- app/main.py
+++ app/main.py
@@ -5,8 +5,12 @@ class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> object:
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+    def __exit__(self,
+                 exc_type: Exception,
+                 exc_val: Exception,
+                 exc_tb: Exception,
+                 ) -> None:
         os.remove(self.filename)
