e8b979b2f7217a0e66c494fb8480cd322c944206
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-02-28 17:53:43 -0600

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, file_name: str) -> None:
+        self.filename = file_name
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            os.remove(self.filename)
+        except FileNotFoundError:
+            print("File doesn't exist!")
+        return self
