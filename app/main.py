6cf0270d361d5ae5f68a6f96612dc0c5edae7c2a
Vitalii Shkrobatskyi <vetali5700@gmail.com>
2023-04-03 22:26:07 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
+from typing import TextIO
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+        self.file = None
+
+    def __enter__(self) -> TextIO:
+        self.file = open(self.filename, "w")
+        return self
+
+    def __exit__(
+            self,
+            exc_type: Exception,
+            exc_val: Exception,
+            exc_tb: Exception
+    ) -> None:
+        self.file.close()
+        return os.remove(self.filename)
