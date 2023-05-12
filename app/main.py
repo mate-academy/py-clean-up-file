8d24560f19527e32d09382d4e3d2243ffd917f14
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-05-12 17:45:17 +0300

    ..

--- app/main.py
+++ app/main.py
@@ -2,13 +2,13 @@ import os
 
 
 class CleanUpFile:
-    def __init__(self, filename: str):
+    def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> None:
         self.file = open(self.filename, "w")
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type: str, exc_val: str, exc_tb: str) -> None:
         self.file.close()
         os.remove(self.filename)
