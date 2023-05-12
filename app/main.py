3bb1c1009cb8567e2bf19e88e7ebcbf6ade4673b
admin <admin@Yaroslavs-MacBook-Pro.local>
2023-05-12 18:00:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,22 @@
+import os
+from typing import Optional, Type
+from types import TracebackType
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> "CleanUpFile":
+        return self
+
+    def __exit__(self,
+                 exc_type: Optional[Type[BaseException]],
+                 exc: Optional[BaseException],
+                 traceback: Optional[TracebackType]) -> None:
+        try:
+            remove_file = open(self.filename, "r")
+            remove_file.close()
+            os.remove(self.filename)
+        except FileNotFoundError:
+            pass
