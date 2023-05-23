22821033e3e48a70f69ae21fa009f06109a5dcf9
Vitaliy Perehrest <vit.perehrest@gmail.com>
2023-05-23 12:02:15 +0300

    clean_up_file

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
+from __future__ import annotations
+from types import TracebackType
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> CleanUpFile:
+        return self
+
+    def __exit__(self, exc_type: type[BaseException],
+                 exc_val: BaseException,
+                 exc_tb: TracebackType) -> None:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
