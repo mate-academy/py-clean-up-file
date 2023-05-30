f62d5ecacaa699417cf85c03fe8be0eeb4e18f70
S-SMTN <cven28@gmail.com>
2023-05-30 16:12:59 +0300

    implemented CleanUpFile

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
+from typing import TextIO
+from os import remove
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str, mode: str = "r") -> None:
+        self.mode = mode
+        self.filename = filename
+        self.file = None
+
+    def __enter__(self) -> TextIO:
+        self.file = open(self.filename, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.file.close()
+        remove(self.filename)
