7c717c52d2ae14b7b680bc635f5ce0893f3487a0
Makeyev Yevhen <makeyev.ye@gmail.com>
2023-06-06 15:56:42 +0300

    Task complete

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
+from __future__ import annotations
+from os.path import exists
+from os import remove
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, file_name: str) -> None:
+        self.filename = file_name
+
+    def __enter__(self) -> CleanUpFile:
+        return self
+
+    def __exit__(self, *_) -> None:
+        if exists(self.filename):
+            remove(self.filename)
