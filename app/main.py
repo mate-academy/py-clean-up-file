f2570e1f3dd1e065d74f3a5a237b65dfc00bfec1
Anton Komarov <anton_komarov_qa@ukr.net>
2023-06-11 14:25:48 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
+from __future__ import annotations
+from os import remove
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> CleanUpFile:
+        return self
+
+    def __del__(self) -> None:
+        remove(self.filename)
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.__del__()
