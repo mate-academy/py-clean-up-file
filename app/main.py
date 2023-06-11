b8ede47cb040de39c95759c9a4473d4e8e37f20d
Anton Komarov <anton_komarov_qa@ukr.net>
2023-06-11 14:29:38 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -9,5 +9,5 @@ class CleanUpFile:
     def __enter__(self) -> CleanUpFile:
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+    def __exit__(self, *args) -> None:
         remove(self.filename)
