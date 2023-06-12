168919430135ad6eae71a9c0dd7ec4f6850cc59e
Olha Tryhub <olha.tryhub@nure.ua>
2023-06-12 21:30:16 +0300

    Solution for Clean Uo File

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
+from __future__ import annotations
+from typing import Any
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> CleanUpFile:
+        return self
+
+    def __exit__(self,
+                 file_type: type,
+                 value: Any,
+                 traceback: Exception) -> None:
+
+        os.remove(self.filename)
