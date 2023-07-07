f6faaab4ee8bb4216f41d429f42fe1bc48a7152c
Yegor Turenko <ytferma@gmail.com>
2023-07-07 18:27:11 +0300

    v1

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
+import os
+from typing import Type
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> callable:
+        return self
+
+    def __exit__(
+            self,
+            exc_type: Type[BaseException],
+            exc_val: BaseException,
+            exc_tb: str
+    ) -> None:
+        os.remove(self.filename)
