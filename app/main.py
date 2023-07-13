888c0d8927a6a85e92aa465ae3620996b936c06e
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2023-07-13 14:15:57 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
+from __future__ import annotations
+from types import TracebackType
+from typing import Type
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> CleanUpFile:
+        return self
+
+    def __exit__(self,
+                 exc_type: Type[BaseException] | None,
+                 exc_val: BaseException | None,
+                 exc_traceback: TracebackType | None) -> None:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+        else:
+            print("File is not present in the system.")
