9db49f65b2545cdc3921dd1f36e7775f1fc5518f
Viktor Deyneka <3lobites@gmail.com>
2023-07-23 21:19:28 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,22 @@
+import os
+from typing import Any
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+        self.mode = "w"
+        self.file = None
+
+    def __enter__(self) -> object:
+        self.file = open(self.filename, self.mode)
+        return self
+
+    def __exit__(
+            self,
+            exc_type: Any,
+            exc_value: Any,
+            exc_traceback: Any
+    ) -> None:
+        self.file.close()
+        os.remove(self.filename)
