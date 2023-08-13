cb24a05246ef3279e5b46b448ebf0d289913fbb4
Kosta Stepanjuk <stepanjukkosta@qmail.com>
2023-08-14 00:32:24 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
+from __future__ import annotations
+from typing import Any
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> CleanUpFile:
+        return self
+
+    def __exit__(self, exc_type: Any, exc_val: Any,
+                 exc_traceback: Any) -> None:
+        if os.path.isfile(self.filename):
+            os.remove(self.filename)
+
+
+with CleanUpFile("file.txt"):
+    with open("file.txt", "w") as file:
+        file.write("Hello Mate!")
