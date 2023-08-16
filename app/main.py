1acc8980ca37084968278f86deab1675703d7708
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-08-16 09:29:52 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
+from __future__ import annotations
+from typing import Any
+
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> CleanUpFile:
+        return self
+
+    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
+        try:
+            os.remove(self.filename)
+            print(f"File '{self.filename}' removed successfully.")
+        except OSError as e:
+            print(f"Error deleting '{self.filename}': {e}")
