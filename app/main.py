2183438c37fda8178768a30d9aac396e42d9b09a
Bohdan Kolomiiets <kb907406@gmail.com>
2023-08-21 12:34:01 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
+from __future__ import annotations
+from typing import Any
+import os.path
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> CleanUpFile:
+        return self
+
+    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+        else:
+            print(f"There isn't file with name {self.filename}")
