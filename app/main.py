6ab22036c55a53a2ffa13465005e79f6884e60b9
Denys <deibukdenyscloud@gmail.com>
2023-08-21 21:29:31 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
+import os
+from types import TracebackType
+from typing import Type
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> "CleanUpFile":
+        return self
+
+    def __exit__(
+        self,
+        exc_type: Type[BaseException] | None,
+        exc_value: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> bool:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+        return False
