a3bede3344fa803e31806890074aa0efd0e27974
oreny <orenycdota@gmail.com>
2023-08-22 16:59:59 +0300

    ANN001 fix

--- app/main.py
+++ app/main.py
@@ -2,13 +2,15 @@ import os
 
 
 class CleanUpFile:
-    def __init__(self, filename) -> None:
+    def __init__(self, filename: str) -> None:
         self.filename = filename
 
     def __enter__(self) -> "CleanUpFile":
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+    def __exit__(
+        self, exc_type: object, exc_val: object, exc_tb: object
+    ) -> None:
         try:
             os.remove(self.filename)
         except FileNotFoundError:
