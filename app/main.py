32b0409a5154f2dcf0b22a6a69dcf13a360963fe
Ostap <ostapbojko04@gmail.com>
2023-08-22 20:35:16 +0300

    py-clean

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> None:
+        open("filename", "w")
+        return self
+
+    def __exit__(
+            self,
+            type_exception: str,
+            value: str,
+            traceback: str
+    ) -> None:
+        if os.path.isfile(self.filename):
+            os.remove(self.filename)
