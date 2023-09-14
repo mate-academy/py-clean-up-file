822cada432ef69bf496242827c2c2f5197438bb5
Misha Shushman <endernagashushman@gmail.com>
2023-09-14 20:22:36 +0300

    soltion

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
-class CleanUpFile:
-    # write your code here
+from os import remove, path
+
+
+class Clean:
     pass
+
+
+class CleanUpFile(Clean):
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    # How to set self annotation? When i set CleanUpFile python raise NameError
+    def __enter__(self) -> Clean:
+        return self
+
+    def __exit__(self, exc_type: str, exc_val: str, exc_tb: str) -> None:
+        if path.exists(self.filename):
+            remove(self.filename)
