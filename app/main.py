603cf33ab04326b93f13336c9114aee3895d194e
Dmytro Prokhach <dprokhach@gmail.com>
2023-10-02 21:42:39 +0300

    Added little changes

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(name: str) -> None:
+        self.name = name
+    
+    def __enter__(self):
+        pass
+
+    def __exit__(self):
+        pass
+    
+    def __del__(self):
+        del(self.name)
+      
