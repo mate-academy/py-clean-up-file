a4ac9f0ed82790150c5330e1d33c8f2d15aef71d
Dmytro Prokhach <dprokhach@gmail.com>
2023-10-02 21:53:31 +0300

    Added little change

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 class CleanUpFile:
-    def __init__(name: str) -> None:
-        self.name = name
+    def __init__(file_name: str, method: str) -> None:
+        self.file_obj = open(file_name, method)
     
     def __enter__(self):
-        pass
+        return self.file_obj
 
     def __exit__(self):
-        pass
+        self.file_obj.close()
     
     def __del__(self):
-        del(self.name)
+        del(self.file_obj)
       
