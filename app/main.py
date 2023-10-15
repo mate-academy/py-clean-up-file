0dc5d4b0fd63e37e5123a4a3026a8075506646ea
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-10-15 13:45:50 +0300

    Fix error

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 from __future__ import annotations
 
 import os
+from typing import Any
 
 
 class CleanUpFile:
     def __init__(self, file_name: str) -> None:
-        self.file_name = file_name
+        self.filename = file_name
 
     def __enter__(self) -> CleanUpFile:
         return self
 
-    def __exit__(self) -> None:
-        os.remove(self.file_name)
+    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
