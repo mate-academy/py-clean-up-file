6ff7de41318702ec5529f0fa29e41a31a18d9281
Oleksandr Pupchenko <oleksandr.pupchenko@gmail.com>
2023-10-18 02:15:52 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> None:
+        return self
+
+    def __exit__(self, exc_type: str, exc_val: str, exc_tb: str) -> None:
+        os.remove(self.filename)
