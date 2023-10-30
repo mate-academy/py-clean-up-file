3e508b163aeee4ae7598b0af1aba400a663ad5f5
Andrii Homeniuk <andreygomenuk@gmail.com>
2023-10-30 23:35:06 +0200

    Implemented the CleanUpFile context manager as a class.

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
+from os import remove, path
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, file_name: str) -> None:
+        self.file_name = file_name
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if path.exists(self.file_name):
+            remove(self.file_name)
