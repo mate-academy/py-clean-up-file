cf5895ce52371ee53bfaae56fc9f5397b7f2b673
Andrii Homeniuk <andreygomenuk@gmail.com>
2023-10-30 23:43:51 +0200

    Added annotations.

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+from types import TracebackType
+from typing import Optional, Type
 from os import remove, path
 
 
@@ -5,9 +8,14 @@ class CleanUpFile:
     def __init__(self, file_name: str) -> None:
         self.file_name = file_name
 
-    def __enter__(self):
+    def __enter__(self) -> CleanUpFile:
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(
+            self,
+            exc_type: Optional[Type[BaseException]],
+            exc_val: Optional[BaseException],
+            exc_tb: Optional[TracebackType]
+    ) -> None:
         if path.exists(self.file_name):
             remove(self.file_name)
