5ab89cc3b9fcb05786dbf9055fc9b4a6d808dca4
Sergii Pryima <spryima@gmail.com>
2023-11-01 18:03:01 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
+from typing import Generator, Optional, Type
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> Generator:
+        return self
+
+    def __exit__(self,
+                 exc_type: Optional[Type[BaseException]],
+                 exc_val: Optional[BaseException],
+                 exc_tb: Optional[Type[BaseException]]) -> None:
+        path = os.path.abspath(self.filename)
+        os.remove(path)
