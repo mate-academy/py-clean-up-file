19112a911ebcc2a1667709c2c77e3bd8197f61f6
Aeros72 <pashamelnyk2004@gmail.com>
2023-11-04 15:13:34 +0200

    Implemented py-clean-up-file

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
+from __future__ import annotations
+from types import TracebackType
+from typing import Optional, Type
+from os import path, remove
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> CleanUpFile:
+        return self
+
+    def __exit__(
+            self,
+            exc_type: Optional[Type[BaseException]],
+            exc_val: Optional[BaseException],
+            exc_tb: Optional[TracebackType]
+    ) -> None:
+        if path.exists(self.filename):
+            remove(self.filename)
