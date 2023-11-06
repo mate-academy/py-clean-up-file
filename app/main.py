c0855dc7bc8c4e083ed94732ba51432f7c790f82
Roman Shupak <romanshupak808@gmail.com>
2023-11-06 20:11:51 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> object:
+        return self
+
+    def __exit__(self, exc_type: str, exc_val: str, exc_tb: str) -> None:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+
+
+if __name__ == "__main__":
+    with CleanUpFile("file.txt") as manager:
+        with open("file.txt", "w") as new_file:
+            new_file.write("Hello, Mate!")
