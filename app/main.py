5836968c89e90d403be052b47a534ca32110a07e
Alex <oleksa.shcherbakov@gmail.com>
2023-11-08 21:15:06 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
+import os
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, file_name, mode):
+        self.file_name = file_name
+        self.mode = mode
+        self.file = None
+
+    def __enter__(self):
+        self.file = open(self.file_name, self.mode)
+        return  self.file
+
+    def __exit__(self,exc_type, exc_value, exc_traceback):
+        self.file.close()
+
+    def __del__(self):
+        os.remove(self.file_name)
+
+
+with CleanUpFile("test.txt", "w") as file:
+    file.write("!!!!")
