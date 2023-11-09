9b6f720035ce7cb8234e888f0eb87fd5951116bd
Yuliia Dudych <juliadudych@gmail.com>
2023-11-09 21:54:26 +0200

    Add missing annotations

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+from typing import Optional, Type
+from types import TracebackType
 import os
 
 
@@ -5,8 +8,13 @@ class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> CleanUpFile:
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(
+            self,
+            exc_type: Optional[Type[BaseException]],
+            exc_value: Optional[BaseException],
+            traceback: Optional[TracebackType]
+    ) -> None:
         os.remove(self.filename)
