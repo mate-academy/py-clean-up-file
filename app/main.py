21a1172c16c57fa93cf0d56bbaa651eb87cd70b6
lyaskov <lyaskov1@gmail.com>
2023-11-19 18:35:33 +0200

    fixed flake8 error

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
+from __future__ import annotations
+
 import os
+from typing import Optional, Type, Any
 
 
 class CleanUpFile:
-    def __init__(self, filename: str):
+    def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> CleanUpFile:
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type: Optional[Type[BaseException]],
+                 exc_val: Optional[BaseException],
+                 exc_tb: Optional[Any]) -> None:
         os.remove(self.filename)
