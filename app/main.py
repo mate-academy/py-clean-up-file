dd5cc0cc2e4c033add6235d243b4ef4035438d14
Liliya <liliyakhu@ukr.net>
2023-11-25 18:16:02 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,25 @@
+from __future__ import annotations
+
+import os
+
+from types import TracebackType
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> CleanUpFile:
+        return self
+
+    def __exit__(
+            self,
+            exc_type: type[BaseException] | None,
+            exc_value: BaseException | None,
+            exc_traceback: TracebackType | None
+    ) -> None:
+        pass
+
+    def __del__(self) -> None:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
