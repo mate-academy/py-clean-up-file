cdbc64d9cc9771192122bfc08e7da47cdd0b9fa3
Yuliia Valko <yuliia.valko1@gmail.com>
2023-11-27 15:05:38 +0200

    Silution_2

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import os
 
 
@@ -7,12 +8,12 @@ class CleanUpFile:
         self.filename = filename
         self.file = None
 
-    def __enter__(self):
+    def __enter__(self) -> CleanUpFile:
         if self.filename:
             self.file = open(self.filename, "w")
             return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type: any, exc_val: any, exc_tb: any) -> None:
         if self.file:
             self.file.close()
             os.remove(self.filename)
