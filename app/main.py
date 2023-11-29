e6d8a74d4066209df9ce5e5117900a0597f86a4a
Ryshtun Denys <rishtund@gmail.com>
2023-11-29 14:08:36 +0200

    ADone

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
+from __future__ import annotations
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str, mode: str = "w") -> None:
+        self.filename = filename
+        self.mode = mode
+        self.file = None
+
+    def __enter__(self) -> callable:
+        self.file = open(self.filename, self.mode)
+        return self
+
+    def __exit__(self, exc_type: any, exc_val: any, exc_tb: str) -> None:
+        self.file.close()
+        os.remove(self.filename)
