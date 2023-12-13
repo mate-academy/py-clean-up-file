fb213d7e52f2a157813ab7aea0cc6c2ad429fb99
Borys Kirichenko <boryskirichenko99@gmail.com>
2023-12-13 20:34:24 +0100

    all tests passed

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
