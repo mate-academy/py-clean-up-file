a4034cf1cc26ae01d721225127fd4f15c3ba24e8
Borys Kirichenko <boryskirichenko99@gmail.com>
2023-12-14 20:24:41 +0100

    flake8 fixed

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
+from typing import Any
 import os
 
 
 class CleanUpFile:
-    def __init__(self, filename):
+    def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> Any:
         return self
 
-    def __exit__(self, type, value, traceback):
+    def __exit__(self, exc_type: None, exc_val: None, exc_tb: None) -> None:
         if os.path.exists(self.filename):
             os.remove(self.filename)
