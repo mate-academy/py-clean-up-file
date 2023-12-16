eda3959208435dc63e110812cb2818904fe73682
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2023-12-17 00:22:22 +0200

    feat: trash

--- app/main.py
+++ app/main.py
@@ -6,7 +6,16 @@ class CleanUpFile:
         self.filename = filename
 
     def __enter__(self) -> None:
-        return self
+        try:
+            print(open(self.filename, "r"), "content")
+        except FileNotFoundError as error:
+            print(error, "error")
+            return None
 
     def __exit__(self) -> None:
         os.remove(self.filename)
+
+
+with CleanUpFile("file.txt"):
+    with open("file.txt", "w") as file:
+        file.write("Hello Mate!")
