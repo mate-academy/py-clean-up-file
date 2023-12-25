4855c865198062b51f8debeeebf9e47ef4e1b99c
Dmytro Gertsovenko <gertsovenko@gmail.com>
2023-12-26 00:01:32 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
+from __future__ import annotations
+from os import remove, path
+from typing import Optional, Type
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> CleanUpFile:
+        return self
+
+    def __exit__(self,
+                 exception_type: Optional[Type[BaseException]],
+                 exception_value: Optional[BaseException],
+                 traceback: Optional[Type[BaseException]]
+                 ) -> None:
+        if path.exists(self.filename):
+            remove(self.filename)
