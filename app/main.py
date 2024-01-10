4d56688a2c55a51cb8ed6cbc7e99584b3b81a0eb
Vasyl Kalish <vasyl.documents@yahoo.it>
2024-01-10 21:44:58 +0100

    fix task

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
 import os
+from typing import Any
 
 
 class CleanUpFile:
     def __init__(self, file_name: str) -> None:
         self.filename = file_name
 
-    def __enter__(self):
+    def __enter__(self) -> None:
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type: Any,
+                 exc_val: type,
+                 exc_tb: str) -> None:
         pass
 
-    def __del__(self):
+    def __del__(self) -> None:
         os.remove(self.filename)
