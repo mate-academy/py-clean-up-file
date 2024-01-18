594787de9256e9d39911023a8004da8a7bf74b86
Ant <u123@ua.fm>
2024-01-18 23:03:04 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,20 @@
+from __future__ import annotations
+import os.path
+
+
 class CleanUpFile:
     # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> CleanUpFile:
+        return self
+
+    def __exit__(
+        self,
+        exc_type: BaseException,
+        exc_val: BaseException,
+        exc_tb: object,
+    ) -> None:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
