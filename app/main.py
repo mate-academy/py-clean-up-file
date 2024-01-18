d67949eb6616a9a4aac643269a4b874367383db1
yes <yes@yess-MacBook-Pro.local>
2024-01-18 05:57:26 +0200

    done

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, file_name: str) -> None:
+        self.file_name = file_name
+
+    def __enter__(self)-> None:
+        return self
+
+    def __exit__(self, exc_type: str, exc_val: str, traceback: str) -> None:
+        os.remove(self.file_name)
