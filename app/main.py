4b028b23af7dbcdb967328a4d272bec3589f03fd
yes <yes@yess-MacBook-Pro.local>
2024-01-18 06:01:34 +0200

    done3

--- app/main.py
+++ app/main.py
@@ -3,10 +3,10 @@ import os
 
 class CleanUpFile:
     def __init__(self, file_name: str) -> None:
-        self.file_name = file_name
+        self.filename = file_name
 
     def __enter__(self) -> None:
         return self
 
     def __exit__(self, exc_type: str, exc_val: str, traceback: str) -> None:
-        os.remove(self.file_name)
+        os.remove(self.filename)
