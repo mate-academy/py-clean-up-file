df9828bb657b12008f6a6ea5f0988140e119779f
yes <yes@yess-MacBook-Pro.local>
2024-01-18 05:58:48 +0200

    done

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ class CleanUpFile:
     def __init__(self, file_name: str) -> None:
         self.file_name = file_name
 
-    def __enter__(self)-> None:
+    def __enter__(self) -> None:
         return self
 
     def __exit__(self, exc_type: str, exc_val: str, traceback: str) -> None:
