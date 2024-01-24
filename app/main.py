8e402c4ce61fb8257a90ee7855c63a74924ef11f
unknown <artem.sapiens@gmail.com>
2024-01-24 17:38:50 +0200

    'Fix-bag'

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 import os
 
+
 class CleanUpFile:
-    def __init__(self, filename):
+    def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> None:
         return self
 
-    def __exit__(self, exc_type, exc_value, traceback):
+    def __exit__(self, exc_type: str, exc_value: str, traceback: str) -> None:
         if os.path.exists(self.filename):
             os.remove(self.filename)
 
