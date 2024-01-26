08515c5c693fcebd94e3455d06ed34a71ca39691
Ded <71114713+DenZatolokin@users.noreply.github.com>
2024-01-26 14:31:14 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 class CleanUpFile:
-    def __init__(self, file_name, method):
-        self.file_obj = open(file_name, method)
+    def __init__(self, file_name: str, method: str) -> None:
+        self.file_obj: TextIO = open(file_name, method)
         
-    def __enter__(self):
+    def __enter__(self) -> TextIO:
         return self.file_obj
         
-    def __exit__(self, type, value, traceback):
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
         self.file_obj.close()
