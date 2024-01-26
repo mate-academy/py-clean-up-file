fa35817e3d23847803ee87115c1d09d51186049a
Ded <71114713+DenZatolokin@users.noreply.github.com>
2024-01-26 14:27:36 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,9 @@
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, file_name, method):
+        self.file_obj = open(file_name, method)
+        
+    def __enter__(self):
+        return self.file_obj
+        
+    def __exit__(self, type, value, traceback):
+        self.file_obj.close()
