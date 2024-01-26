f3ace4361b21e331255055b3b457633967064006
Ded <71114713+DenZatolokin@users.noreply.github.com>
2024-01-26 14:35:34 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from typing import TextIO
 class CleanUpFile:
     def __init__(self, file_name: str, method: str) -> None:
         self.file_obj: TextIO = open(file_name, method)
-    def __enter__(self) -> :
+    def __enter__(self) -> TextIO:
         return self.file_obj
     def __exit__(self) -> None:
         self.file_obj.close()
