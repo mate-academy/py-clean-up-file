bbf637b0d0d4620f22d8297abaecdbd244eb9f65
sergey <38cerg12021979@jmal.com>
2024-01-29 19:17:08 +0200

    29_01_24

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
+import os
+import traceback
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> object:
+        return self
+
+    def __exit__(
+            self, exc_type: type,
+            exc_val: Exception,
+            exc_tb: traceback
+    ) -> None:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
