2f1e362a656f91fe95a9c48c7a2efc559c22bbae
Halyna Baklanova <novabalka@gmail.com>
2024-02-07 10:43:20 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,20 @@
+from pathlib import Path
+from typing import Optional, Type, Any
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> None:
+        return self
+
+    def __exit__(
+            self,
+            exc_type: Optional[Type[BaseException]],
+            exc_val: Optional[BaseException],
+            exc_tb: Optional[Any]
+    ) -> None:
+        file_path = Path(self.filename)
+        if file_path.exists():
+            file_path.unlink()
