f00ecab85789d86376d09644c9fcaa7ae958ebe2
Molchanov Ihor <=>
2024-02-11 23:01:55 +0200

    solved task

--- app/main.py
+++ app/main.py
@@ -1,3 +1,23 @@
+from __future__ import annotations
+from os import remove
+from types import TracebackType
+from typing import Type, Optional
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+        self.file = None
+
+    def __enter__(self) -> CleanUpFile:
+        self.file = open(file=self.filename, mode="w")
+        return self
+
+    def __exit__(
+            self,
+            exc_type: Type[BaseException],
+            exc_val: Optional[BaseException],
+            exc_tb: Optional[TracebackType]
+    ) -> None:
+        self.file.close()
+        remove(self.filename)
