e76112afc1d772bb42908df1ba1af2e18beb6447
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2024-02-12 16:58:50 +0200

    Fix

--- app/main.py
+++ app/main.py
@@ -10,6 +10,6 @@ class CleanUpFile:
     def __enter__(self) -> CleanUpFile:
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+    def __exit__(self, exc_type: None, exc_val: None, exc_tb: None) -> None:
         if os.path.exists(self.filename):
             os.remove(self.filename)
