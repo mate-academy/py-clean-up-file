8f5e6ca530e7f9965e1fac860bf625eb5d16284b
Liliya <liliyakhu@ukr.net>
2024-03-12 15:17:24 +0200

    Solution with enter and exit

--- app/main.py
+++ app/main.py
@@ -9,16 +9,16 @@ class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    # def __enter__(self) -> CleanUpFile:
-    #     return self
-    #
-    # def __exit__(
-    #         self,
-    #         exc_type: type[BaseException] | None,
-    #         exc_value: BaseException | None,
-    #         exc_traceback: TracebackType | None
-    # ) -> None:
-    #     pass
+    def __enter__(self) -> CleanUpFile:
+        return self
+
+    def __exit__(
+            self,
+            exc_type: type[BaseException] | None,
+            exc_value: BaseException | None,
+            exc_traceback: TracebackType | None
+    ) -> None:
+        pass
 
     def __del__(self) -> None:
         if os.path.exists(self.filename):
