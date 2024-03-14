b766ced8fc7a59d0824918822f15c847a7708cfe
Viesich <v.s.vesich@gmail.com>
2024-03-14 23:42:20 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,24 @@
+from __future__ import annotations
+import traceback
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> CleanUpFile:
+        return self
+
+    def __exit__(
+            self,
+            exc_type: type,
+            exc_val: Exception,
+            exc_tb: traceback
+    ) -> None:
+        try:
+            with open(self.filename, "r") as f:
+                f.read()
+            os.remove(self.filename)
+        except FileNotFoundError:
+            pass
