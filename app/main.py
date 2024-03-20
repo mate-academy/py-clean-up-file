f68e4573ba409f95697a496696963c0937cf2f98
Kate Arzhevikina <arzhevikina96@gmail.com>
2024-03-20 18:28:47 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,25 @@
+from __future__ import annotations
+import os
+import pathlib
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> CleanUpFile:
+        self.file = open(f"{self.filename}", "w")
+        return self
+
+    def __exit__(
+            self,
+            exc_type: object = None,
+            exc_value: object = None,
+            exc_traceback: object = None
+    ) -> None:
+        self.path = pathlib.Path(f"{self.filename}")
+        try:
+            self.file.close()
+            os.remove(self.path)
+        except FileNotFoundError:
+            pass
