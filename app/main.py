1451b6fc01df8b317017e2361189753a5ef87af2
Futilite <marketesh@gmail.com>
2024-03-26 23:48:30 +0200

    CleanFile_1

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
+import os
+from typing import Optional
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> "CleanUpFile":
+        return self
+
+    def __exit__(self,
+                 exc_type: Optional[type],
+                 exc_value: Optional[BaseException],
+                 traceback: Optional[type]
+                 ) -> None:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
