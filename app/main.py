271ec8f5d472ca34258948689068f4a0c87a6ef2
Marharyta Marchenko <ritanika12@gmail.com>
2024-03-29 16:35:27 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
+from typing import Optional, Type, Any, Union
 from pathlib import Path
+import types
 
 
 class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self) -> 'CleanUpFile':
+    def __enter__(self) -> "CleanUpFile":
         return self
 
-    def __exit__(self, exc_type, exc_value, traceback) -> None:
+    def __exit__(self, exc_type: Optional[Type[BaseException]],
+                 exc_value: Optional[BaseException],
+                 traceback: Optional[Union[types.TracebackType, Any]]) -> None:
         file_path = Path(self.filename)
         if file_path.exists():
             file_path.unlink()
