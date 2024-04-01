f020ad2f5a0c27eaf1e6308e5cab7a2728341b71
Maksym Hekalo <maximgekalo@gmail.com>
2024-04-01 07:55:44 +0300

    create CleanUpFile context manager class

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
+from os import remove
+from typing import Any
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+
+    def __init__(self, filename: str) -> None:
+        self.file_object = None
+        self.filename = filename
+
+    def __enter__(self) -> object:
+        try:
+            self.file_object = open(self.filename, "w")
+        finally:
+            return self
+
+    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
+        self.file_object.close()
+        remove(self.filename)
