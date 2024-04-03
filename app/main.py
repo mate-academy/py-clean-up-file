fc54e3ca435fc2c5bde61e2e73af0146cc0d76b8
Liliia <furman06228@gmail.com>
2024-04-03 08:12:50 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,25 @@
+from typing import Optional, Type, Any
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> "CleanUpFile":
+        return self
+
+    def __exit__(self,
+                 exc_type: Optional[Type[BaseException]],
+                 exc_value: Optional[BaseException],
+                 traceback: Optional[Any]) -> bool:
+        try:
+            import os
+            os.remove(self.filename)
+            print(1)
+        except FileNotFoundError:
+            pass
+
+
+with CleanUpFile("file.txt"):
+    with open("file.txt", "w") as file:
+        file.write("Hello Mate!")
