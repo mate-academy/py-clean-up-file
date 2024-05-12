b471a543da3e98de753785dd7c1fb01f6ccc17ad
Andrew Chugreev <andrey.4.1985@gmail.com>
2024-05-12 16:57:27 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
 from __future__ import annotations
+from typing import Optional, Type
 import os
 
 
 class CleanUpFile:
-    def __init__(self, filename) -> None:
+    def __init__(self, filename: str) -> None:
         self.filename = filename
 
     def __enter__(self) -> CleanUpFile:
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+    def __exit__(
+            self,
+            exc_type: Optional[Type[BaseException]],
+            exc_val: Optional[Type[BaseException]],
+            exc_tb: Optional[Type[BaseException]]
+    ) -> None:
         os.remove(self.filename)
