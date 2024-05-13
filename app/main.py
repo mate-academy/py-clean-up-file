b9c149f1eda46eca280e40cd014af3ca646774c4
Anna Shevtsova <aniashevtsova@gmail.com>
2024-05-13 10:42:00 +0200

    all tests pass

--- app/main.py
+++ app/main.py
@@ -1,3 +1,20 @@
+import os
+from typing import Type
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> "CleanUpFile":
+        print("Enter method called")
+        return self
+
+    def __exit__(
+            self,
+            exc_type: Type[BaseException],
+            exc_val: BaseException,
+            exc_tb: Type
+    ) -> None:
+        print("Exit method called")
+        os.remove(self.filename)
