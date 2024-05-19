b040ebdeb169569167bf22fac763fab27683f989
RuslanLevchuk <rusik.levchuk.m@gmail.com>
2024-05-19 21:12:54 +0300

    Hell yeah solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,22 @@
+from typing import Callable
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str, mode: str = "w") -> None:
+        self.filename = filename
+        self.mode = mode
+        self.file = None
+
+    def __enter__(self) -> object:
+        self.file = open(self.filename, self.mode)
+        return self
+
+    def __exit__(
+            self,
+            exc_type: Exception,
+            exc_val: Exception,
+            exc_tb: Callable
+    ) -> None:
+        self.file.close()
+        os.remove(self.filename)
