84205fc0adfcbcff54cc54782a4fc6a27cfca9df
Sivkovich Danylo <danilsivkovic@gmail.com>
2024-06-17 20:06:55 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,22 @@
+from os import remove
+from os.path import exists
+from typing import Optional, Type, Any
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> object:
+        return self
+
+    def __exit__(self, exc_type: Optional[Type[BaseException]],
+                 exc_val: Optional[BaseException],
+                 exc_tb: Optional[Any]) -> None:
+        if exists(self.filename):
+            remove(self.filename)
+
+
+with CleanUpFile("file.txt"):
+    with open("file.txt", "w") as file:
+        file.write("Hello Mate!")
