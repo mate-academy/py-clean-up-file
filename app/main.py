ea8a7c1be29245c2c8cb994d9541484c7dd4c486
Ruslan Samsonovych <arankutakepakuta@gmail.com>
2024-06-19 20:36:23 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 import os
-from typing import Self, Any
+from typing import Any
 
 
 class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self) -> Self:
+    def __enter__(self) -> Any:
         return self
 
     def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
