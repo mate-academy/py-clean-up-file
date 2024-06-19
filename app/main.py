1a7f0612e6b66f882449af5fe20e0c80317109ca
Ruslan Samsonovych <arankutakepakuta@gmail.com>
2024-06-19 20:33:00 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
+import os
+from typing import Self, Any
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> Self:
+        return self
+
+    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+
+
+with CleanUpFile("kex.txt"):
+    with open("kex.txt", "w") as file:
+        file.write("Hello Mate!")
