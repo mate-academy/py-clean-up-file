1b89d0ab8525b1838c437c70ec3b40ffd8a69700
Vadym Piddubnyi <leegrad@ukr.net>
2024-07-08 22:24:46 -0500

    Solution

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ class CleanUpFile:
     def __enter__(self) -> None:
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+    def __exit__(self, exc_type: None, exc_val: None, exc_tb: None) -> None:
         if os.path.exists(self.filename):
             os.remove(self.filename)
             print(f"File '{self.filename}' has been removed.")
