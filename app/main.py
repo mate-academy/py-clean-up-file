863d474bf6f536687e82ea8f9035e8459344de66
viktor-77 <vumanskiy0664041341@gmail.com>
2024-07-16 23:28:16 +0300

    Resolved "Сlean Up File" tasl

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
+from __future__ import annotations
+
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str):
+        self.filename = filename
+        self.file = None
+
+    def __enter__(self) -> CleanUpFile:
+        self.file = open(self.filename, "w")
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+        os.remove(self.filename)
