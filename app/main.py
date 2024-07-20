ee16e7f0556f4e75af0bc19ac77d4c76ce2a37e1
Anton Bilous <anton.bilosus@gmail.com>
2024-07-20 11:55:45 +0300

    Implement context manager

--- app/main.py
+++ app/main.py
@@ -1,3 +1,22 @@
+import os
+from typing import Type
+from types import TracebackType
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> "CleanUpFile":
+        return self
+
+    def __exit__(
+        self,
+        exc_type: Type[BaseException],
+        exc_value: BaseException,
+        exc_traceback: TracebackType,
+    ) -> None:
+        try:
+            os.unlink(self.filename)
+        except FileNotFoundError:
+            pass
