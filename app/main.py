e3f3b8f460ea72bee9bd38821bc9e1c74b34c2d0
andreykolomiec <akolomiec982@gmail.com>
2024-07-23 23:25:26 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,19 +1,24 @@
 import os
+from typing import Optional, Type
 
 
 class CleanUpFile:
-    def __init__(self, namefile: str) -> None:
-        self.namefile = namefile
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
 
-    def __enter__(self) -> str:
-        return self.namefile
+    def __enter__(self) -> "CleanUpFile":
+        return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
-        if os.path.exists(self.namefile):
-            os.remove(self.namefile)
+    def __exit__(
+            self, exc_type: Optional[Type[BaseException]],
+            exc_val: Optional[Type[BaseException]],
+            exc_tb: Optional[Type[BaseException]]
+    ) -> bool:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
         return False
 
 
-with CleanUpFile("file.txt"):
-    with open("file.txt", "w") as file:
+with CleanUpFile("file.txt") as clean_up:
+    with open(clean_up.filename, "w") as file:
         file.write("Hello Mate!")
