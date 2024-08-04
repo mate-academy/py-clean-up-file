872f9020f6408d4de38e57cfa38a520e45c2e9a8
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-08-05 00:55:45 +0300

    add CleanUpFile class

--- app/main.py
+++ app/main.py
@@ -5,10 +5,10 @@ class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> None:
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type: None, exc_val: None, exc_tb: None) -> bool:
         if os.path.exists(self.filename):
             os.remove(self.filename)
 
