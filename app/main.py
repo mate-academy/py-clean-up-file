0fafd9c8fbd71cca7b5f6e1c013b27443131b35f
Yehor <sizngg9@gmail.com>
2024-08-07 22:52:49 +0300

    flake8 fix

--- app/main.py
+++ app/main.py
@@ -1,14 +1,20 @@
 from os.path import exists
 from os import remove
+from typing import Any, Optional, Type
 
 
 class CleanUpFile:
-    def __init__(self, filename: str):
+    def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> Any:
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(
+            self,
+            exc_type: Optional[Type[BaseException]],
+            exc_val: Optional[BaseException],
+            exc_tb: Optional[Any]
+    ) -> None:
         if exists(self.filename):
             remove(self.filename)
