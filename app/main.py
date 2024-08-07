0327bd550292e05f58eb5298c589e721d6e4bf31
Yehor <sizngg9@gmail.com>
2024-08-07 18:47:04 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
+from os.path import exists
+from os import remove
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str):
+        self.filename = filename
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exists(self.filename):
+            remove(self.filename)
