503fef8fb1d6eef2a2b6e7e4c487b2e05ddcaf01
FalseCoreCentury <simonov0101k@gmail.com>
2024-08-12 19:30:31 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -10,7 +10,12 @@ class CleanUpFile:
     def __enter__(self) -> Any:
         return self
 
-    def __exit__(self, ecx_type: str, exc_value: str, exc_traceback: str) -> Any:
+    def __exit__(
+            self,
+            ecx_type: str,
+            exc_value: str,
+            exc_traceback: str
+    ) -> Any:
         if os.path.isfile(self.filename):
             os.remove(self.filename)
         return False
