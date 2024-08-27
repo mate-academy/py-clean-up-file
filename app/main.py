f31593782d328a771f01da3d1fb3a1686f0dec05
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-08-27 17:41:32 +0300

    code refactor by flake8

--- app/main.py
+++ app/main.py
@@ -3,14 +3,14 @@ import os
 
 
 class CleanUpFile:
-    def __init__(self, filename) -> None:
+    def __init__(self, filename: str) -> None:
         self.filename = filename
-
 
     def __enter__(self) -> Any:
         return self
 
-
-    def __exit__(self, exc_type, exc_value, exc_traceback) -> Any:
+    def __exit__(self, exc_type: Exception,
+                 exc_value: Exception,
+                 exc_traceback: Exception) -> Any:
         if os.path.exists(self.filename):
             os.remove(self.filename)
