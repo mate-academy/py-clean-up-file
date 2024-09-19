8fa3000cdd1c70fa921aae3c6657a77d42ee8605
Alona <alena.tsapenko7@gmail.com>
2024-09-19 16:31:29 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,23 @@
+import os
+from typing import Optional, Type, Any
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> "CleanUpFile":
+        return self
+
+    def __exit__(
+            self,
+            exc_type: Optional[Type[BaseException]],
+            exc_value: Optional[BaseException],
+            traceback: Optional[Any]
+    ) -> None:
+        try:
+            os.remove(self.filename)
+        except FileNotFoundError:
+            pass
+        except (OSError, IOError):
+            print(f"Failed to delete file: {self.filename}")
