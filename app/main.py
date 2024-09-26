75d5726ac9df45cecb17efaf7eb8a649b23288ca
yura <pollo7964132@gmail.com>
2024-09-26 17:11:58 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,24 @@
+from __future__ import annotations
+import os
+from typing import Any
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+        self.file = None
+
+    def __enter__(self) -> CleanUpFile:
+        self.file = open(self.filename, "w")
+        return self
+
+    def __exit__(
+            self,
+            exc_type: Any,
+            exc_val: Any,
+            exc_tb: Any
+    ) -> None:
+        self.file.close()
+
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
