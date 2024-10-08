aaa682d7a190c72e154b95630e96fe0c4d6d5513
yuliiapyrih <yuliia.pyrih.pm.2018@lpnu.ua>
2024-10-08 22:33:47 +0300

    implement py-clean-up-file

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
+from __future__ import annotations
+from typing import Optional, Type
+from types import TracebackType
+
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> CleanUpFile:
+        return self
+
+    def __exit__(
+            self,
+            exc_type: Optional[Type[BaseException]],
+            exc_value: Optional[BaseException],
+            traceback: Optional[TracebackType]
+    ) -> None:
+        os.remove(self.filename)
