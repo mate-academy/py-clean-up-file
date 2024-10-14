fde75e76ded158413ed4a25d309137a624745e86
Dmytro Stepanenko <dmytrostepanenkoua@gmail.com>
2024-10-14 14:24:40 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
+from typing import Self
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> Self:
+        return self
+
+    def __exit__(self, *files) -> None:
+        import os
+
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+            print(f"Файл {self.filename} успішно видалено.")
+        else:
+            print(f"Файл {self.filename} не знайдено.")
