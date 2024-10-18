06ccfac3a863153ae45124823135727148f653e0
ABC <matweyes@gmail.com>
2024-10-18 12:23:46 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
+from __future__ import annotations
+from typing import Any
 import os
 
 
 class CleanUpFile:
-    def __init__(self, filename: str):
+    def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> CleanUpFile:
         return self
 
-    def __exit__(self, exc_type, exc_value, traceback):
+    def __exit__(self, exc_type: Any, exc_value: Any, traceback: Any) -> None:
         if os.path.exists(self.filename):
             os.remove(self.filename)
             print(f"file '{self.filename}' was deleted.")
