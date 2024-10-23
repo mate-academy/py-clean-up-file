db3eacf552ba3c4aab8e9ac1428a7ce13814575b
srx <srx92@protonmail.com>
2024-10-23 17:20:33 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,20 @@
+from __future__ import annotations
+from os import remove
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(
+            self,
+            filename: str
+    ) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> CleanUpFile:
+        return self
+
+    def __exit__(
+            self,
+            exc_type: str,
+            exc_val: str,
+            exc_tb: str) -> None:
+        remove(self.filename)
