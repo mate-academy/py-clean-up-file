27e0c145d79b00287f99ceadc91c7d45538c2c91
bashyrov <bashyroov@gmail.com>
2024-11-13 23:54:27 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
+import os
+from typing import Any
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, file_name: str) -> None:
+        self.file_name = file_name
+
+    def __enter__(self) -> Any:
+        self.file = open(self.file_name, "w")
+        return self.file_name
+
+    def __exit__(self, exc_type: type, exc_val: str,
+                 exc_tb: BaseException) -> None:
+        self.file.close()
+        os.remove(self.file_name)
