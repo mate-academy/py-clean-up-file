f86071d6fee796c976498674bff798e93af70536
bashyrov <bashyroov@gmail.com>
2024-11-13 23:56:18 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -3,14 +3,14 @@ from typing import Any
 
 
 class CleanUpFile:
-    def __init__(self, file_name: str) -> None:
-        self.file_name = file_name
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
 
     def __enter__(self) -> Any:
-        self.file = open(self.file_name, "w")
-        return self.file_name
+        self.file = open(self.filename, "w")
+        return self.filename
 
     def __exit__(self, exc_type: type, exc_val: str,
                  exc_tb: BaseException) -> None:
         self.file.close()
-        os.remove(self.file_name)
+        os.remove(self.filename)
