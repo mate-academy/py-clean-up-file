8fd2890d9a515eac5543f7409dcff1432808fd9a
Volodymyr Povroznyk <vpovroznyk1998@gmail.com>
2024-11-20 01:35:01 +0200

    fix2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from os import remove
-from traceback import TracebackException
+from types import TracebackType
 from typing import Any, Optional, Type
 
 
@@ -7,17 +7,14 @@ class CleanUpFile:
 
     def __init__(self, filename: str) -> None:
         self.filename = filename
-        self.file = None
 
     def __enter__(self) -> Any:
-        self.file = open(self.filename, "w")
         return self
 
     def __exit__(
             self,
             exc_type: Optional[Type[BaseException]],
             exc_value: Optional[BaseException],
-            exc_traceback: Optional[TracebackException]
+            exc_traceback: Optional[TracebackType]
     ) -> None:
-        self.file.close()
         remove(self.filename)
