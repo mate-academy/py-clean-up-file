8583a53cde21e1c93addfa9a0e812280f6e3e4ce
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2024-11-20 17:04:03 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
+from os import remove, path
+from typing import Any
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+        self.mode = "w"
+        self.file = None
+
+    def __enter__(self) -> Any:
+        self.file = open(self.filename, self.mode)
+        return self
+
+    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
+        self.file.close()
+        if self.file is not None:
+            remove(path.abspath(self.filename))
