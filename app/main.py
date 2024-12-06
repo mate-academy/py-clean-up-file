c886e09337db777d5a7a0f7e5c54e2a165c89b73
Vlad Tymoshko <slradbez@gmail.com>
2024-12-06 21:03:47 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> None:
+        # Метод __enter__ ничего не делает в данном случае
+        return self
+
+    def __exit__(self, exc_type: BaseException,
+                 exc_value: BaseException,
+                 traceback: Exception) -> None:
+        # Метод __exit__ выполняет удаление файла, если он существует
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
