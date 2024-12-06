f15e8695856ed835530c5630835cf63b62e7415b
Vlad Tymoshko <slradbez@gmail.com>
2024-12-06 21:04:51 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -6,12 +6,10 @@ class CleanUpFile:
         self.filename = filename
 
     def __enter__(self) -> None:
-        # Метод __enter__ ничего не делает в данном случае
         return self
 
     def __exit__(self, exc_type: BaseException,
                  exc_value: BaseException,
                  traceback: Exception) -> None:
-        # Метод __exit__ выполняет удаление файла, если он существует
         if os.path.exists(self.filename):
             os.remove(self.filename)
