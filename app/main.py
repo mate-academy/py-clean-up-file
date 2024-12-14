2c740948ae9cd4298ff48f2c16de1ae12a6121da
artem <krenky.spenky@gmail.com>
2024-12-14 16:15:29 +0200

    'sol'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
+import os.path
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, file_name: str) -> None:
+        self.file_name = file_name
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if os.path.exists(self.file_name):
+            os.remove(self.file_name)
+            print(f"File '{self.file_name}' has been deleted.")
