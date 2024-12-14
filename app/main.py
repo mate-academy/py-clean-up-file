5a72d41f959ced939e5330e7811016337b3b0ca3
artem <krenky.spenky@gmail.com>
2024-12-14 16:19:28 +0200

    'sol'

--- app/main.py
+++ app/main.py
@@ -5,10 +5,15 @@ class CleanUpFile:
     def __init__(self, file_name: str) -> None:
         self.file_name = file_name
 
-    def __enter__(self):
+    def __enter__(self) -> "CleanUpFile":
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(
+            self,
+            exc_type: BaseException,
+            exc_val: BaseException,
+            exc_tb: type
+    ) -> None:
         if os.path.exists(self.file_name):
             os.remove(self.file_name)
             print(f"File '{self.file_name}' has been deleted.")
