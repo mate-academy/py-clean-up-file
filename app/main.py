9914296240e368df1dd4133d6ed92fc1bf2d0e36
IvankaKuzin <kuziniwanka@gmail.com>
2024-12-15 22:39:48 +0200

    Changed annotations

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
+from __future__ import annotations
+
 import os
+from typing import Any
 
 
 class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> CleanUpFile:
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
         os.remove(self.filename)
