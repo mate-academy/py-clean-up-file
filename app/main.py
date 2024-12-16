c62f5118ef93fb22760a66d75cd23f05cfdaf8a1
Max <makimov2004@gmail.com>
2024-12-16 15:10:42 +0200

    CleanUpFile

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, file_name: str) -> None:
+        self.filename = file_name
+
+    def __enter__(self) -> None:
+        return self
+
+    def __exit__(self, exc_type: type, exc_value: Exception,
+                 traceback: object) -> None:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
