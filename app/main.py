9a17f426da7207ab3c24cca5ab1a4378ffb2de00
Andrew <andrewsiemakin@gmail.com>
2024-12-16 18:46:04 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -9,7 +9,10 @@ class CleanUpFile:
     def __enter__(self) -> "CleanUpFile":
         return self
 
-    def __exit__(self, exc_type: Optional[Type[BaseException]], exc_val: Optional[BaseException],
-                 exc_tb: Optional[object]) -> None:
+    def __exit__(
+            self,
+            exc_type: Optional[Type[BaseException]],
+            exc_val: Optional[BaseException],
+            exc_tb: Optional[object]) -> None:
         if os.path.exists(self.filename):
             os.remove(self.filename)
