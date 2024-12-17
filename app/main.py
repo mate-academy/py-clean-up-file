448555d6e8ee95dca71dc653b3e3ab239098a7dc
Dmytro <dima.sichkar2003@gmail.com>
2024-12-17 17:18:44 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
+from __future__ import annotations
+from typing import Any
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> CleanUpFile:
+        return self
+
+    def __exit__(self, exc_type: Any, exc_value: Any, traceback: Any) -> None:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+            print(f"File '{self.filename}' has been removed.")
+        else:
+            print(f"File '{self.filename}' does not exist.")
