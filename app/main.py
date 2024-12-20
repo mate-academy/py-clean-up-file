3e6de9dd3a1953005cf662afe0070553ada39c55
neeeksa <avgust.k31@gmail.com>
2024-12-21 01:03:46 +0200

    first_solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
+import os
+
+from typing import TextIO, Optional
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+        self.file: Optional[TextIO] = None
+
+    def __enter__(self) -> "CleanUpFile":
+        self.file = open(self.filename, "w")
+        return self
+
+    def __exit__(self, exc_type: None, exc_val: None, exc_tb: None) -> None:
+        if self.file:
+            self.file.close()
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
