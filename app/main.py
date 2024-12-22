7c44dc7cf049d07b3fbdb989246a4c60fd23a14e
JustVov4uk <volodabudzan4@gmail.com>
2024-12-22 16:34:50 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
+import os
+from inspect import Traceback
+from typing import Optional
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> None:
+        return self
+
+    def __exit__(self, exc_type: Optional[type[BaseException]],
+                 exc_val: Optional[BaseException],
+                 exc_tb: Optional[Traceback]) -> None:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+        else:
+            print(f"{self.filename} is not exist.")
