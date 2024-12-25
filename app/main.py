9f06ed905f645359fb2ab26455b0d6609fd7d189
Liza Shulika <liza.shulika2009@gmail.com>
2024-12-25 16:21:43 +0200

    clean1

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
+import os
+
+
 class CleanUpFile:
-    def __init__(self, name: str) -> None:
-        self.name = name
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
         self.file = None
 
-    def __enter__(self):
-        self.file = open(self.name)
+    def __enter__(self) -> "CleanUpFile":
+        self.file = open(self.filename, "w")
+        return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
-
-    def __del__(self):
+    def __exit__(self, exc_type: object,
+                 exc_val: object, exc_tb: object) -> None:
         self.file.close()
+
+    def __del__(self) -> None:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
