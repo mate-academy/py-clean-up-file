514558c20efa16c09cab9f5d49ef8d33645464d4
Liza Shulika <liza.shulika2009@gmail.com>
2024-12-25 15:15:01 +0200

    clean

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.file = None
+
+    def __enter__(self):
+        self.file = open(self.name)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def __del__(self):
+        self.file.close()
