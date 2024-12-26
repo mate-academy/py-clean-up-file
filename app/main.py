e1a225dc23c3d94d24057a73daf72f91cfa46f19
maksymtrokhymchuk <maximuschampion2002@gmail.com>
2024-12-26 16:20:01 +0200

    cleane-up-file

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> "CleanUpFile":
+        return self
+
+    def __exit__(self, exc_type: str, exc_val: str, exc_tb: str) -> None:
+        try:
+            if os.path.exists(self.filename):
+                os.remove(self.filename)
+                print(f"File {self.filename} has been removed.")
+        except Exception as e:
+            print(f"Error: {e}")
