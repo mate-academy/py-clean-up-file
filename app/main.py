f900db9354fd130924680549b4174c6594767d4d
JulianskyMe <juliakamynskaya@gmail.com>
2024-12-30 20:45:34 +0200

    upd

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename: str = filename
 
-    def __enter__(self) -> 'CleanUpFile':
+    def __enter__(self) -> "CleanUpFile":
         return self
 
     def __exit__(self, exc_type: type,
