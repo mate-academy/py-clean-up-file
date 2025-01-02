851195dab8c89200e0984d6311576dca920e379a
Greshch <greshchook@gmail.com>
2025-01-02 18:53:31 +0200

    __enter__ return type fail

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
+import _io
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+        self.file = None
+
+
+    def __enter__(self, mode: str = "w") -> _io.TextIOWrapper:
+        self.file = open(self.filename, mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+        os.remove(self.filename)
+
+
