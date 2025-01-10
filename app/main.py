38ff03a0b9994c4dd1a6bf5b095ae54b6f486439
Olha Yukhnenko <pyolh36@gmail.com>
2025-01-10 19:37:09 +0200

    remove_type_import_fix_characters

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import os
-from typing import Optional, Type
+from typing import Optional
 from types import TracebackType
 
 
@@ -10,7 +10,12 @@ class CleanUpFile:
     def __enter__(self) -> "CleanUpFile":
         return self
 
-    def __exit__(self, exc_type: Optional, exc_val: Optional, exc_tb: TracebackType) -> bool:
+    def __exit__(
+            self,
+            exc_type: Optional,
+            exc_val: Optional,
+            exc_tb: TracebackType
+    ) -> bool:
         if os.path.exists(self.filename):
             os.remove(self.filename)
         return False
