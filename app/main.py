c7354cede08ba8c6ade57611e53716297b20b504
Olha Yukhnenko <pyolh36@gmail.com>
2025-01-10 19:35:27 +0200

    add_type_annotation_

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 import os
+from typing import Optional, Type
+from types import TracebackType
 
 
 class CleanUpFile:
     def __init__(self, filename: str = "file.txt") -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> "CleanUpFile":
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type: Optional, exc_val: Optional, exc_tb: TracebackType) -> bool:
         if os.path.exists(self.filename):
             os.remove(self.filename)
         return False
