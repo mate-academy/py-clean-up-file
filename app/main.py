b44f82b5596d4611a11c989da070a484a9c41321
Pavlo Danylkiv <danylkiv.pavlo@gmail.com>
2025-01-17 20:46:47 +0000

    completed task

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
+from __future__ import annotations
+from os import remove, path
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> CleanUpFile:
+        return self
+
+    def __exit__(self,
+                 exc_type: ValueError | TypeError,
+                 exc_val: str | None,
+                 exc_tb: bool) -> None:
+        if hasattr(self, "file"):
+            self.file.close()
+        if path.exists(self.filename):
+            remove(self.filename)
