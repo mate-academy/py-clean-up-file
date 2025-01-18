3832a71349ac7d8d917104eccfdcba01236da577
Atom-gnomov <gorbach0864213579@gmail.com>
2025-01-18 11:36:07 +0200

    completed

--- app/main.py
+++ app/main.py
@@ -1,3 +1,22 @@
+import os
+from typing import Optional, Type
+from types import TracebackType
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+        self.mode = "w"
+        self.file = None
+
+    def __enter__(self) -> "CleanUpFile":
+        self.file = open(self.filename, self.mode)
+        return self
+
+    def __exit__(self, exc_type: Optional[Type[BaseException]],
+                 exc_value: Optional[BaseException],
+                 traceback: Optional[TracebackType]) -> None:
+        self.file.close()
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
