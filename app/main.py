4f49b974be2ffee508fbc85834bc589b0a62f816
Oksana <oksana.zhornyk@gmail.com>
2025-01-23 13:00:44 +0100

    Solution py-clean-up-file

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
+import os
+from traceback import TracebackException
+from types import TracebackType
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+        self.file = None
+
+    def __enter__(self) -> "CleanUpFile":
+        self.file = open(self.filename, "w")
+        return self
+
+    def __exit__(self, exc_type: Exception,
+                 exc_val: TracebackException, exc_tb: TracebackType) -> None:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
