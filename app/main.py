ea4c4a1c4272299c7c28b6264635df174aafcf5c
Mykyta <mitina1900@gmail.com>
2025-01-29 13:15:36 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
+from pathlib import Path
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self):
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        file_path = Path(self.filename)
+        if file_path.exists():
+            file_path.unlink()
