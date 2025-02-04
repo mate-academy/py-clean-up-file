78bb7b9a89e6cdc715b2c7ef9031c1190a1fadb8
Odin-max <meh84741@gmail.com>
2025-02-04 20:44:09 +0200

    1234

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 import os
-
+from typing import Any
 
 class CleanUpFile:
-    def __init__(self, filename):
+    def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> Any:
         return self
 
-    def __exit__(self):
+    def __exit__(self) -> Any:
         if os.path.exists(self.filename):
             os.remove(self.filename)
