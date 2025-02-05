37d6a65e593dc64b0be59d715d0475af0faba0f8
Ruslan Priadko <r.priadko@ukr.net>
2025-02-05 13:00:42 +0200

    solution with typing

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import os
+from typing import Any
 
 
 class CleanUpFile:
@@ -6,10 +7,11 @@ class CleanUpFile:
         self.file_name = filename
         self.file_obj = open(self.file_name)
 
-    def __enter__(self):
+    def __enter__(self) -> Any:
         return self.file_obj
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type: Any,
+                 exc_val: Any,
+                 exc_tb: Any) -> None:
         self.file_obj.close()
         os.remove(self.file_name)
-
