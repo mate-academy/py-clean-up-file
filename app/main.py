24a0a2e9849167b9690fb2a407a59b347b9debd6
Ruslan Priadko <r.priadko@ukr.net>
2025-02-05 13:18:29 +0200

    errors addressed

--- app/main.py
+++ app/main.py
@@ -4,14 +4,17 @@ from typing import Any
 
 class CleanUpFile:
     def __init__(self, filename: str) -> None:
-        self.file_name = filename
-        self.file_obj = open(self.file_name)
+        self.filename = filename
+        self.file_obj = open(filename, "w")
 
     def __enter__(self) -> Any:
-        return self.file_obj
+        return self
 
     def __exit__(self, exc_type: Any,
                  exc_val: Any,
                  exc_tb: Any) -> None:
         self.file_obj.close()
-        os.remove(self.file_name)
+        try:
+            os.remove(self.filename)
+        except FileNotFoundError:
+            pass
