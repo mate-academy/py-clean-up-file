540d84cfb6170e284be936f4d0a6ba2f6dcd5492
Ksenia Malashchuk <malashchuk.ksenia@gmail.com>
2025-02-11 22:58:44 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,25 @@
+import os
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> "CleanUpFile":
+        return self
+
+    def __exit__(
+            self,
+            exc_type: None,
+            exc_value: None,
+            traceback: object
+    ):
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+            print(f"File '{self.filename}' has been deleted.")
+        else:
+            print(f"File '{self.filename}' does not exist.")
+
+with CleanUpFile("file.txt"):
+    with open("file.txt", "w") as file:
+        file.write("Hello Mate!")
+    print("File created and written to.")
