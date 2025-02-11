88ceac7fe9af6baf0f6ca53c331e325d4087d82c
Ksenia Malashchuk <malashchuk.ksenia@gmail.com>
2025-02-11 23:03:56 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,17 +5,15 @@ class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-
     def __enter__(self) -> "CleanUpFile":
         return self
-
 
     def __exit__(
             self,
             exc_type: None,
             exc_value: None,
             traceback: object
-    ):
+    ) -> None:
         if os.path.exists(self.filename):
             os.remove(self.filename)
             print(f"File '{self.filename}' has been deleted.")
