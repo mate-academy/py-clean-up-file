8b16008c8e30bfb01a33950ccbe74bf70e78b947
nikita-plotnytskyi <nikita.plotnitckiy@gmail.com>
2025-02-12 15:11:47 +0200

    fix annotationÐ

--- app/main.py
+++ app/main.py
@@ -1,19 +1,18 @@
 import os
-from typing import Self, Optional, Type
 
 
 class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self) -> Self:
+    def __enter__(self) -> object:
         return self
 
     def __exit__(
         self,
-        exc_type: Optional[Type[BaseException]],
-        exc_val: Optional[BaseException],
-        exc_tb: Optional[object]
+        exc_type: object,
+        exc_val: object,
+        exc_tb: object
     ) -> None:
         pass
 
