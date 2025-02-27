c4e18fd8d07567f6c03596752e42356685884561
Andrii <yurkivandriy02@gmail.com>
2025-02-27 16:32:43 +0200

    Completed task clean-up-file

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename):
+        self.filename = filename
+        self.file = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type=None, exc_val=None, exc_tb=None):
+        if self.file:
+            self.file.close()
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
